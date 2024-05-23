c607b7be919614ce894dd9b96e4a8ff0c129003e
Anurag <anuragawasthi020@gmail.com>
2024-05-23 18:51:28 +0530

    Initial commit

---
+++ test/aws-test-application.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsTestApplication from '../lib/aws-test-application-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-test-application-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsTestApplication.AwsTestApplicationStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
